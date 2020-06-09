e170aac90e5e9ad441f261b59367f556314f15ad
francisco28 <fgallegosgo@gmail.com>
2020-06-08 18:51:50 -0600

    add Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,41 @@ import React from 'react';
 
 const Form = () => {
     return ( 
-        <h1>Form here</h1>
+        <form 
+            className="col-12"
+        >
+            <fieldset className="text-center">
+                <legend>Search drinks for category or ingredient</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input 
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Search for ingredient"
+                    />
+                </div>
+                
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="category"
+                    >
+                        <option value="">--Select Category--</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input 
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Search Drinks"
+                    />
+                </div>
+            </div>
+        </form>
      );
 }
  
